3cccb61513b0408e953ff53194d9c442ff72520a
civ <Civiled>
2025-01-12 09:35:31 -0800

    Updated core scripts Started adding additional utility scripts

--- dbg.py
+++ dbg.py
@@ -3,7 +3,7 @@ import os
 from threading import Thread
 
 class pwnio:
-    def __init__(self, binary, use_remote: bool, use_debugger: bool, debugger="r2", remote_ip="", remote_port=-1, libc=None, ld=None):
+    def __init__(self, binary, use_remote: bool, use_debugger: bool, debugger="r2", remote_ip="", remote_port=-1, libc=None, ld=None, terminal=["alacritty", "-e"]):
         self.binary = binary
         self.use_remote = use_remote
         self.use_debugger = use_debugger
@@ -12,6 +12,7 @@ class pwnio:
         self.remote_port = remote_port
         self.libc = libc
         self.ld = ld
+        self.terminal = terminal
     
     def connect(self):
         if self.use_remote:
@@ -26,65 +27,50 @@ class pwnio:
                 self.io = process(self.binary.path)
         return self.io
     
-    def debug(self, script=""):
+    def debug(self, r2_script=None, gdb_script=None):
         if self.use_debugger:
             if not self.use_remote: 
                 if self.debugger == "r2":
-                    attachr2(self.io, script)
+                    r2 = r2Attach(self.io, self)
+
+                    def dbg(r2, dbgscript=None):
+                        r2.attach(dbgscript)
+
+                    dbg_thread = Thread(target = dbg, args=[r2, r2_script])
+                    dbg_thread.start()
                 elif self.debugger == "gdb":
-                    gdb.attach(self.io, script)
+                    gdb.attach(self.io, gdb_script)
                 input("Press Enter to continue...")
             else:
                 print("Not attaching debugger on remote...")
 
-# R2ATTACH
-def attachr2(io, r2_script=None):
-    r2 = r2Attach(io, terminal=["alacritty", "-e"])
-
-    def dbg(r2, dbgscript=None):
-        print("Attaching with script:", dbgscript)
-        r2.attach(dbgscript)
-
-    dbg_thread = Thread(target = dbg, args=[r2, r2_script])
-    dbg_thread.start()
-
 class r2Attach:
-    def __init__ (self, process, terminal=None):
+    def __init__ (self, process, ctx):
         if isinstance(process, pwnlib.tubes.process.process):
             self.pid = process.pid
-            self.debuggee = process.program
+            self.target = process.program
         else:
             raise Exception ("Insert a correct pwntools process object")
         
-        # Terminal preferences
-        if terminal != None:
-            self.terminal = terminal
-        else:
-            self.terminal = ["tmux", 'splitw', '-h']
-
-        self.r2template ="""#!/usr/bin/env python
+        self.ctx = ctx
+            
+    def attach(self, r2script=None):
+        script_template = """#!/usr/bin/env python
 import os, r2pipe
 r2 = r2pipe.open()
-
-def load_modules():
-    modules = r2.cmdj("dmmj")
-    for module in modules:
-        if '{mod_name:s}' == os.path.basename(module['file']):
-            command = "oba {{addr:d}} {{file_name:s}}".format(file_name=module['file'], addr=module['address'])
-            r2.cmd(command)
-
-load_modules()
-r2.cmd('ib') # Reload the buffer info
-
-{user_commands:s}"""
-    def attach(self, r2script=None):
+"""
         if r2script != None:
-            script_file = os.path.join ("/tmp/", self.debuggee + ".py")
-            with open(script_file,"w+") as f:
-                f.write(self.r2template.format(mod_name=os.path.basename(self.debuggee), user_commands=r2script))
-            command = self.terminal + ['r2', "-i", script_file, "-d", str(self.pid)]
+            x = r2script.split("\n")
+            for y in x:
+                if y == "":
+                    continue
+                script_template += f"print(r2.cmd('{y}'), end='')\n"
+            
+            script_template += "r2.quit()\n"
+            with open('/tmp/pwn.py', 'w') as file:
+                file.write(script_template)
+            command = self.ctx.terminal + ['r2', '-i', '/tmp/pwn.py', '-d', str(self.pid)]
         else:
-            command = self.terminal + ['r2', '-d', str(self.pid)]
-        
-        subprocess.call (command)
+            command = self.ctx.terminal + ['r2', '-d', str(self.pid)]
+        subprocess.call(command)
         
